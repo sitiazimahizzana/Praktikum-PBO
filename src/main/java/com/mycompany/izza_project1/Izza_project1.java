0d6fe4faaddccdb4b8885140c191d161397453bf
Siti Azimah Izzana <siti.azimah.izzana@students.untidar.ac.id>
2026-08-30 11:58:12 +0700

    Initial commit - tugas biodata

---
+++ src/main/java/com/mycompany/izza_project1/Izza_project1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.izza_project1;
+
+/**
+ *
+ * @author user
+ */
+public class Izza_project1 {
+    public static void main(String[] args){
+        System.out.println("Nama: Siti Azimah Izzana");
+        System.out.println("NPM: 2505060013");
+        System.out.println("Alamat: Rembang");
+        System.out.println("No_Hp: 085802853050");
+    }
+}
